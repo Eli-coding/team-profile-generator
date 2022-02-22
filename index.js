909651619c80447c0aacf99d238507edc83122cf
Elizabeth Rodriguez <elizabethyoannie@outlook.com>
2022-02-21 22:38:54 -0400

    HTML bones made

---
+++ index.js
@@ -0,0 +1,7 @@
+const inquirer = require("inquirer");
+const employee = require ("./lib/Employee.js");
+const manager = require ("./lib/Manager.js");
+const engineer = require ("./lib/Emgineer.js");
+const intern = require ("./lib/Intern.js"); 
+
+
